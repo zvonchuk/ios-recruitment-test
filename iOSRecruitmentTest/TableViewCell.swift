9a8cbfe8cf38559094c998a0bdd15dc6d4f583d4
Bazyli Zygan <bazyl@novaproject.net>
2016-06-15 11:07:07 +0200

    Initial test project structure

---
+++ iOSRecruitmentTest/TableViewCell.swift
@@ -0,0 +1,39 @@
+//
+//  TableViewCell.swift
+//  iOSRecruitmentTest
+//
+//  Created by Bazyli Zygan on 15.06.2016.
+//  Copyright © 2016 Snowdog. All rights reserved.
+//
+
+import UIKit
+
+class TableViewCell: UITableViewCell {
+
+    var item: AnyObject? {
+        didSet {
+            if item == nil {
+                iconView.image = nil
+                itemTitleLabel.text = "Test"
+                itemDescLabel.text = "Some description"
+            } else {
+                // TODO: Implement item sets
+            }
+        }
+    }
+    
+    @IBOutlet weak var iconView: UIImageView!
+    @IBOutlet weak var itemTitleLabel: UILabel!
+    @IBOutlet weak var itemDescLabel: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        iconView.layer.cornerRadius = 4
+    }
+    
+    override func prepareForReuse() {
+        super.prepareForReuse()
+        self.item = nil
+    }
+    
+}
---
+++ iOSRecruitmentTest/ViewController.swift
@@ -0,0 +1,37 @@
+//
+//  ViewController.swift
+//  iOSRecruitmentTest
+//
+//  Created by Bazyli Zygan on 15.06.2016.
+//  Copyright © 2016 Snowdog. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController, UITableViewDataSource, UISearchBarDelegate {
+
+    @IBOutlet weak var searchBar: UISearchBar!
+    @IBOutlet weak var tableView: UITableView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+        tableView.registerNib(UINib(nibName: "TableViewCell", bundle: nil), forCellReuseIdentifier: "TableViewCell")
+    }
+
+
+    // MARK: - UITableView data source
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 20
+    }
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell =  tableView.dequeueReusableCellWithIdentifier("TableViewCell") as! TableViewCell
+        
+        cell.item = nil
+        
+        return cell
+    }
+    
+}
